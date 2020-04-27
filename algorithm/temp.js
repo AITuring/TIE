f23f51a3c365a255cac1dd11b10e7c55f5be12b3
AITuring <shspku365@gmail.com>
2020-04-27 23:58:16 +0800

    33

--- algorithm/temp.js
+++ algorithm/temp.js
@@ -1,4 +1,46 @@
-function mergesort (arr){
-    
+// var arr = [1, [2, 3, [4, 5, [6]]], 7];
 
+
+// function getNewArr(arr) {
+//     var newArr = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         if (typeof arr[i] == "number") {
+//             newArr.push(arr[i])
+//         } else {
+
+//             getNewArr(arr[i])
+//         }
+//     }
+//     return newArr;
+// }
+// console.log(getNewArr(arr))
+
+// console.log(newArr)
+
+a=[1, -2, 3, 10, -4, 7, 2, -5]
+function maxsum(arr) {
+    if (arr.length<0){
+        return false;
+    }
+    if (arr.length==0){
+        return 0;
+    }
+    if (arr.length==1){
+        return arr[0];
+    }
+    var cur=arr[0];
+    var ans=arr[0];
+    for(var i=1;i<arr.length;i++) {
+        if(cur <= 0) {
+            cur = arr[i];
+        } else {
+            cur+= arr[i];
+        }
+        if(cur>ans) {
+            ans = cur;
+        }
+    }
+    return ans;
 }
+
+console.log(maxsum(a));
--- algorithm/temp.py
+++ algorithm/temp.py
@@ -1,32 +1,3 @@
-import random
-def random_int_list(start, stop, length):
-    start, stop = (int(start), int(stop)) if start <= stop else (int(stop), int(start))
-    length = int(abs(length)) if length else 0
-    random_list = []
-    for i in range(length):
-        random_list.append(random.randint(start, stop))
-    return random_list
-
-def mergesort(array):
-    # 先分
-    mid=len(array)//2
-    left=array[0:mid]
-    right=array[mid:]
-    left.sort()
-    right.sort()
-    # merge
-    l=0
-    r=0
-    ans=[]
-    while l<len(left) and r<len(right):
-        if left[l]<=right[r]:
-            ans.append(left[l])
-            l+=1
-        else:
-            ans.append(right[r])
-            r+=1
-    return ans
-
-a= random_int_list(1,101,100)
-print(a)
-print(mergesort(a))
+def search(nums, target):
+    # 先二分，二分有一部分有序，我得找出来才行
+    mid=len(nums)//2
